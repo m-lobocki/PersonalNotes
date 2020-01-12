3c8b956165b69d7bca03e283de1346c0643af738
mateusz.lobocki <mateusz.lobocki@blue-veery.com>
2020-01-12 19:38:49 +0100

    Change Task model to interface

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,9 +1,6 @@
-export class Task {
-    constructor(
-        public id: string,
-        public title: string,
-        public description: string,
-        public relatedTasks: Task[]
-    ) {
-    }
+export interface Task {
+    id: string,
+    isDone: boolean,
+    description: string,
+    relatedTasks: Task[]
 }
