8f4f21105254f55403ceb0b44af4cb943a4f8585
mateusz.lobocki <mateusz.lobocki@blue-veery.com>
2020-01-22 09:29:57 +0100

    Add NavigationBar styles Initialize variables.scss

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import {NavLink} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import "./NavigationBar.scss";
 
 export default class NavigationBar extends Component {
     render() {
         return (
             <nav className="navigation-bar">
-                <NavLink className="navigation-bar__brand" exact to="/">Personal Notes</NavLink>
+                <Link className="navigation-bar__brand" to="/">Personal Notes</Link>
                 <NavLink className="navigation-bar__link" activeClassName="navigation-bar__link--active"
                          exact to="/">Home</NavLink>
                 <NavLink className="navigation-bar__link" activeClassName="navigation-bar__link--active"
