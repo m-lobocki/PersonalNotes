cf1e9bdc366d8c5013be41a1b29356587f2ea7a5
Mateusz Łobocki <mlobocki@outlook.com>
2020-01-23 23:56:21 +0100

    Finish ToggleButton component

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,15 +1,24 @@
 import React, {Component, SyntheticEvent} from 'react';
+import './ToggleButton.scss';
+import {c} from "../helpers/class-name";
 
 interface ToggleButtonProps {
-    onToggle: (event: SyntheticEvent, isToggled: boolean) => void;
+    onToggle: (isToggled: boolean) => void;
     isToggled: boolean;
     className?: string;
 }
 
 export default class ToggleButton extends Component<ToggleButtonProps> {
+    emitOnToggle = (event: SyntheticEvent) => {
+        this.props.onToggle(!this.props.isToggled);
+        event.stopPropagation();
+    };
+
     render() {
         return (
-            <button className={this.props.className}>
+            <button
+                className={c`toggle-button ${this.props.className} ${`toggle-button--${this.props.isToggled ? 'activated' : 'deactivated'}`}`}
+                onClick={this.emitOnToggle}>
                 {this.props.children}
             </button>
         );
