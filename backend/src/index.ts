7e40ef91a98c02c067dffc4a4807c338228d5d81
Mateusz Łobocki <mlobocki@outlook.com>
2020-01-31 22:50:31 +0100

    Install and attach express-session

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,13 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import * as express from "express";
 import {Request, Response} from "express";
-import bodyParser = require("body-parser");
+import * as bodyParser from "body-parser";
+import * as session from "express-session";
 
 createConnection().then(async connection => {
     const app = express();
     app.use(bodyParser.json());
+    app.use(session({secret: 'shhhshshshshh', resave: false, saveUninitialized: false}));
     app.listen(3000);
     console.log("Server started");
 }).catch(error => console.log(error));
