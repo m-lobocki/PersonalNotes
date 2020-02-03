3104cfd12ad1a157b92f789b8c1a99e5a0bc405a
Mateusz Łobocki <mlobocki@outlook.com>
2020-02-04 00:32:39 +0100

    Clean App.ts, move endpoints to controller

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,13 +1,10 @@
 import "reflect-metadata";
 import {createConnection, getRepository} from "typeorm";
 import * as express from "express";
-import {Request, Response} from "express";
 import * as bodyParser from "body-parser";
 import * as session from "express-session";
-import User from "./entity/User";
 import {TypeormStore} from "connect-typeorm";
 import Session from "./entity/Session";
-import {CONFLICT, CREATED, FORBIDDEN} from "http-status-codes";
 import {AnnotationMetadata, AnnotationMetadataKey} from "./controllers/Annotations";
 
 export default class App {
@@ -37,45 +34,23 @@ export default class App {
                 ttl: 86400
             }).connect(getRepository(Session))
         }));
-
-        //todo temporary
-        this.express.post('/login', async (req: Request, res: Response) => {
-            const userRepository = getRepository(User);
-            const {email, password} = req.body as User;
-            const user = await userRepository.findOne({where: {email, password}});
-            if (user) {
-                req.session.email = email;
-                res.send();
-            } else {
-                res.status(FORBIDDEN).send();
-            }
-        });
-        this.express.post('/register', async (req: Request, res: Response) => {
-            const userRepository = getRepository(User);
-            const userToRegister = req.body as User;
-            const user = await userRepository.findOne({where: {email: userToRegister.email}});
-            if (user) {
-                res.status(CONFLICT).send();
-            } else {
-                await userRepository.save(userToRegister);
-                res.status(CREATED).send();
-            }
-        });
     }
 
     private initializeControllers(): void {
         for (const controllerConstructor of this.controllers) {
-            const prototype = controllerConstructor.prototype;
-            const annotatedMethods: string[] = Object.getOwnPropertyNames(prototype)
-                .filter(key => typeof prototype[key] === "function")
-                .filter(key => Reflect.hasOwnMetadata(AnnotationMetadataKey, prototype, key));
+            const controllerInstance = new controllerConstructor();
             const controllerRoute: string = Reflect.getMetadata(AnnotationMetadataKey, controllerConstructor);
-            for (const method of annotatedMethods) {
-                const [methodSelector, endpointRoute]: AnnotationMetadata = Reflect.getMetadata(AnnotationMetadataKey, prototype, method);
-                const route = controllerRoute + endpointRoute;
-                // methodSelector(this.express)(route);
-                console.log(route);
-            }
+            const prototype = controllerConstructor.prototype;
+            Object.getOwnPropertyNames(prototype)
+                .filter(key => typeof prototype[key] === "function" && Reflect.hasOwnMetadata(AnnotationMetadataKey, prototype, key))
+                .forEach(method => this.invokeAnnotatedMethod(controllerInstance, method, controllerRoute));
         }
+    }
+
+    private invokeAnnotatedMethod(controllerInstance: any, annotatedMethod: string, controllerRoute: string): void {
+        const [methodSelector, endpointRoute]: AnnotationMetadata = Reflect.getMetadata(AnnotationMetadataKey, controllerInstance, annotatedMethod);
+        const route = controllerRoute + endpointRoute;
+        const controllerMethod: Function = methodSelector(this.express).bind(this.express);
+        controllerMethod(route, controllerInstance[annotatedMethod].bind(controllerInstance));
     }
 }
--- backend/src/controllers/AuthenticationController.ts
+++ backend/src/controllers/AuthenticationController.ts
@@ -1,8 +1,34 @@
 import {Controller, Post} from "./Annotations";
+import {getRepository} from "typeorm";
+import User from "../entity/User";
+import {CONFLICT, CREATED, FORBIDDEN} from "http-status-codes";
+import {Request, Response} from "express";
 
 @Controller('/')
 export default class AuthenticationController {
     @Post('/login')
-    public logIn(): void {
+    public async logIn(request: Request, response: Response): Promise<void> {
+        const userRepository = getRepository(User);
+        const {email, password} = request.body as User;
+        const user = await userRepository.findOne({where: {email, password}});
+        if (user) {
+            request.session.email = email;
+            response.send();
+        } else {
+            response.status(FORBIDDEN).send();
+        }
+    }
+
+    @Post('/register')
+    public async register(request: Request, response: Response): Promise<void> {
+        const userRepository = getRepository(User);
+        const userToRegister = request.body as User;
+        const user = await userRepository.findOne({where: {email: userToRegister.email}});
+        if (user) {
+            response.status(CONFLICT).send();
+        } else {
+            await userRepository.save(userToRegister);
+            response.status(CREATED).send();
+        }
     }
 }
