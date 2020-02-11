20821859cb6d4851f553ab6c02f4067c71a9ddfc
Mateusz Łobocki <mlobocki@outlook.com>
2020-02-11 16:54:15 +0100

    Get rid of progress bar

--- frontend/src/components/tasks/TaskListItem.tsx
+++ frontend/src/components/tasks/TaskListItem.tsx
@@ -5,7 +5,6 @@ import {faChevronDown, faPlus} from "@fortawesome/free-solid-svg-icons";
 import Icon from "../Icon";
 import "./TaskListItem.scss";
 import {c} from "../../helpers/class-name";
-import ProgressBar from "../ProgressBar";
 import {CSSTransition} from "react-transition-group";
 import {connect} from "react-redux";
 import {addTask, updateTask} from "../../redux/actions";
@@ -37,33 +36,11 @@ export class TaskListItem extends React.Component<TaskListItemProps, TaskListIte
         this.props.onTaskUpdate?.(task);
     };
 
-    //todo: in reducer?
-    countRelatedTaskStatuses(task: Task): [number, number] {
-        // let all: number = task.relatedTasks.length;
-        // let done: number = task.relatedTasks.filter(task => task.isDone).length;
-        // for (const relatedTask of task.relatedTasks) {
-        //     const [allRelated, doneRelated] = this.countRelatedTaskStatuses(relatedTask);
-        //     all += allRelated;
-        //     done += doneRelated;
-        // }
-        return [1, 1];
-        // return [all, done];
-    }
-
-    calculateTaskProgress(task: Task): number {
-        const [all, done] = this.countRelatedTaskStatuses(task);
-        if (all === 0) {
-            return 0;
-        }
-        return done * 100 / all;
-    }
-
     render() {
         const relatedTasks: Task[] = this.props.relatedTasks ?? [];
         const hasRelatedTasks: boolean = relatedTasks.length > 0;
         const isExpanded: boolean = this.state.isExpanded;
         const isTaskDone: boolean = this.props.task.isDone;
-        const doneRelatedTaskPercentage: number = this.calculateTaskProgress(this.props.task);
         return (
             <section className="task-list__related-tasks"
                      onClick={this.toggleExpansion}>
@@ -84,7 +61,6 @@ export class TaskListItem extends React.Component<TaskListItemProps, TaskListIte
                         className={c`task-item__expansion-indicator ${`task-item__expansion-indicator--${this.state.isExpanded ? 'activated' : 'deactivated'}`}`}
                         icon={faChevronDown}/>
                     }
-                    <ProgressBar value={doneRelatedTaskPercentage}/>
                 </div>
                 <CSSTransition in={isExpanded} classNames={"task-list"} timeout={300} unmountOnExit>
                     <TaskList parentId={this.props.task.id}/>
