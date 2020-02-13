8cf81348a78cde434160a60c7e91a110bf61213d
Mateusz Łobocki <mlobocki@outlook.com>
2020-02-13 22:19:53 +0100

    Change actions to functions

--- frontend/src/store/tasks/actions.ts
+++ frontend/src/store/tasks/actions.ts
@@ -1,36 +1,31 @@
-import uuid from "uuid";
 import {Task} from "../../models/Task";
-import {Action, ActionCreator} from 'redux';
+import {Action} from 'redux';
 
 export const ADD_TASK = 'ADD_TASK';
 export const LOAD_TASKS = 'LOAD_TASKS';
 export const UPDATE_TASK = 'UPDATE_TASK';
 
 interface AddTaskAction extends Action<typeof ADD_TASK> {
-    id: string;
     task: Task;
 }
 
-export const addTask: ActionCreator<AddTaskAction> = (task: Task) => ({
-    type: ADD_TASK,
-    id: uuid.v4(),
-    task
-});
+export function addTask(task: Task) {
+    return {type: ADD_TASK, task};
+}
 
 interface LoadTasksAction extends Action<typeof LOAD_TASKS> {
 }
 
-export const loadTasks: ActionCreator<LoadTasksAction> = () => ({
-    type: LOAD_TASKS
-});
+export function loadTasks() {
+    return {type: LOAD_TASKS};
+}
 
 interface UpdateTaskAction extends Action<typeof UPDATE_TASK> {
     updatedTask: Task
 }
 
-export const updateTask: ActionCreator<UpdateTaskAction> = (updatedTask: Task) => ({
-    type: UPDATE_TASK,
-    updatedTask
-});
+export function updateTask(updatedTask: Task) {
+    return {type: UPDATE_TASK, updatedTask};
+}
 
 export type TasksActionsTypes = AddTaskAction | LoadTasksAction | UpdateTaskAction;
