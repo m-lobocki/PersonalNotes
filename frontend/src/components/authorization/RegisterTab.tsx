7f6ae92c252d4e9315dab12572cc585683b57f01
mateusz.lobocki <mateusz.lobocki@blue-veery.com>
2020-02-28 08:42:13 +0100

    Add empty auth components

---
+++ frontend/src/components/authorization/RegisterTab.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class RegisterTab extends Component {
+    render() {
+        return (
+            <div className="register">
+            </div>
+        );
+    }
+}
+
+export default RegisterTab;
---
+++ frontend/src/components/authorization/SignInTab.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class SignInTab extends Component {
+    render() {
+        return (
+            <div className="sign-in">
+            </div>
+        );
+    }
+}
+
+export default SignInTab;
