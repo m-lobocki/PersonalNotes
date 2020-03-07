61b366ee09ecdd54d58fa30af0a0cd4f57409ccf
Mateusz Łobocki <mlobocki@outlook.com>
2020-03-08 00:15:14 +0100

    Add possibility to add class name to field

---
+++ frontend/src/components/fields/PasswordField.tsx
@@ -0,0 +1,14 @@
+import React, {Component, HTMLProps, PropsWithoutRef} from 'react';
+import TextField, {TextFieldProps} from "./TextField";
+import './PasswordField.scss';
+
+interface PasswordFieldProps extends TextFieldProps {
+}
+
+export default class PasswordField extends Component<PasswordFieldProps & PropsWithoutRef<HTMLProps<HTMLInputElement>>> {
+    render() {
+        return (
+            <TextField {...this.props} type="password" fieldClassName="password-field"/>
+        );
+    }
+}
--- frontend/src/components/fields/TextField.tsx
+++ frontend/src/components/fields/TextField.tsx
@@ -1,18 +1,21 @@
 import React, {Component, HTMLProps} from 'react';
 import "./TextField.scss";
+import {c} from "../../helpers/class-name";
 
-interface TextFieldProps {
+export interface TextFieldProps {
     label: string;
     id: any;
+    fieldClassName?: string;
 }
 
 export default class TextField extends Component<TextFieldProps & HTMLProps<HTMLInputElement>> {
     render() {
         let props = {...this.props};
-        const {id, label} = props;
+        const {id, label, fieldClassName} = props;
         delete props.label;
+        delete props.fieldClassName;
         return (
-            <div className="text-field">
+            <div className={c`text-field ${fieldClassName}`}>
                 <input className="text-field__input" placeholder=" " {...props}/>
                 <label className="text-field__label" htmlFor={id}>{label}</label>
             </div>
