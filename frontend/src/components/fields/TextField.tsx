9ae0f9f56c4bcfc3977a801427ad0229efa4e0ee
Mateusz Łobocki <mlobocki@outlook.com>
2020-03-08 00:34:43 +0100

    Change floating lable from placeholder-shown to if value empty

--- frontend/src/components/fields/TextField.tsx
+++ frontend/src/components/fields/TextField.tsx
@@ -1,4 +1,4 @@
-import React, {Component, HTMLProps} from 'react';
+import React, {ChangeEvent, Component, createRef, HTMLProps} from 'react';
 import "./TextField.scss";
 import {c} from "../../helpers/class-name";
 
@@ -8,15 +8,31 @@ export interface TextFieldProps {
     fieldClassName?: string;
 }
 
-export default class TextField extends Component<TextFieldProps & HTMLProps<HTMLInputElement>> {
+interface TextFieldState {
+    hasText: boolean;
+}
+
+export default class TextField extends Component<TextFieldProps & HTMLProps<HTMLInputElement>, TextFieldState> {
+    private inputRef = createRef<HTMLInputElement>();
+    readonly state: TextFieldState = {hasText: false};
+
+    inputChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        this.setState({hasText: Boolean(event.target.value)});
+    };
+
     render() {
         let props = {...this.props};
         const {id, label, fieldClassName} = props;
+        const {hasText} = this.state;
         delete props.label;
         delete props.fieldClassName;
         return (
             <div className={c`text-field ${fieldClassName}`}>
-                <input className="text-field__input" placeholder=" " {...props}/>
+                <input
+                    className={c`text-field__input ${{'text-field__input--has-text': hasText}}`}
+                    ref={this.inputRef}
+                    onChange={this.inputChanged}
+                    {...props}/>
                 <label className="text-field__label" htmlFor={id}>{label}</label>
             </div>
         );
