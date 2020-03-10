a7e3da8342b0f4c8f6bb864b73d7bc2a944e2954
Mateusz Łobocki <mlobocki@outlook.com>
2020-03-10 22:54:20 +0100

    Add basic register view

--- frontend/src/components/authorization/RegisterTab.tsx
+++ frontend/src/components/authorization/RegisterTab.tsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import TextField from "../fields/TextField";
+import './RegisterTab.scss';
+import PasswordField from "../fields/PasswordField";
 
 class RegisterTab extends Component {
     render() {
         return (
-            <div className="register">
-            </div>
+            <form className="register">
+                <TextField fieldClassName="email-field" label="Email" id="email"/>
+                <PasswordField fieldClassName="password-field" label="Password" id="password"/>
+                <PasswordField fieldClassName="password-repeat-field" label="Repeat Password" id="repeat-password"/>
+                <button className="button button--primary register-button" type="submit">Register</button>
+            </form>
         );
     }
 }
